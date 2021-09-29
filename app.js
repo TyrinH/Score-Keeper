8841fab35075a86179d7d5b4c5d7354ba80a5680
Tyrin Holloway <tychnk@gmail.com>
2021-09-28 19:37:01 -0500

    added ability to increase each players score via button

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-const playerOne = document.querySelector('#playerOne')
+const p1Value = document.querySelector('#p1Value')
+const p2Value = document.querySelector('#p2Value')
+const p1Button = document.querySelector('#p1Button')
+const p2Button = document.querySelector('#p2Button')
+let p1Score = 0;
+let p2Score = 0;
 
-playerOne.addEventListener('click', function(e) {
-    console.dir(e)
+p1Button.addEventListener('click', function() {
+    p1Score += 1;
+    p1Value.textContent = p1Score;
 })
+
+p2Button.addEventListener('click', function() {
+    p2Score += 1;
+    p2Value.textContent = p2Score;
+})
+
