5578890b472bbaf85e2e09355d62326fa6e3ddd1
Tyrin Holloway <tychnk@gmail.com>
2021-09-29 12:44:56 -0500

    created ability to reset scores

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const p1Value = document.querySelector('#p1Value')
 const p2Value = document.querySelector('#p2Value')
 const p1Button = document.querySelector('#p1Button')
 const p2Button = document.querySelector('#p2Button')
+const reset = document.querySelector('#reset')
 let p1Score = 0;
 let p2Score = 0;
+let winningScore = 5;
 
 p1Button.addEventListener('click', function() {
     p1Score += 1;
@@ -15,3 +17,7 @@ p2Button.addEventListener('click', function() {
     p2Value.textContent = p2Score;
 })
 
+reset.addEventListener('click', function() {
+    p1Score = 0;
+    p2Score = 0;
+})
